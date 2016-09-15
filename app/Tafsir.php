8d9bced464c98272a6371a41a9e4e71a8f9fe3c1
faizalruzky <faizalruzky@gmail.com>
2016-09-15 16:37:17 +0700

    add relation with surah

--- app/Tafsir.php
+++ app/Tafsir.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tafsir extends Model
 {
-    //
+    protected $fillable = ['surat_id','ayat_id','tafsir'];
+    public static function valid(){
+    	return array(
+    		'ayat_id'=>'required',
+    		'tafsir'=>'required'
+    		);
+    }
+    public function Surah(){
+    	return $this->belongsTo('App\Surah','surat_id');
+    }
 }
