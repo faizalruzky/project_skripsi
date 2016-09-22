4b373e5a6f68a5855d97cf35239d2fb5f3d94225
faizalruzky <faizalruzky@gmail.com>
2016-09-22 19:52:23 +0700

    First Commit

---
+++ resources/views/dashboard/shared/footer.blade.php
@@ -0,0 +1,8 @@
+<!-- footer content -->
+       
+          <div class="pull-right">
+            Copyright@2016 - Faizal Ruzky
+          </div>
+          <div class="clearfix"></div>
+     
+        <!-- /footer content -->  
