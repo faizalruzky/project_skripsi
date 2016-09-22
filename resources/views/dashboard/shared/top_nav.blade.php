e917f5e533425c0bd65d9e783b78cd367c7d34b1
faizalruzky <faizalruzky@gmail.com>
2016-09-22 23:04:56 +0700

    add auth

--- resources/views/dashboard/shared/top_nav.blade.php
+++ resources/views/dashboard/shared/top_nav.blade.php
@@ -6,10 +6,12 @@
       </div>
 
       <ul class="nav navbar-nav navbar-right">
+      @if (Auth::guest())
+      <li><a href="{{ url('/login') }}">Login</a></li>
+      @else
         <li class="">
           <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-            <img src="assets/images/picture.jpg" alt="">Faizal Ruzky
-            <span class=" fa fa-angle-down"></span>
+            <img src="assets/images/picture.jpg" alt="">{{Auth::user()->name }}<span class=" fa fa-angle-down"></span>
           </a>
           <ul class="dropdown-menu dropdown-usermenu pull-right">
             <li><a href="javascript:;"> Profile</a></li>
@@ -20,9 +22,10 @@
               </a>
             </li>
             <li><a href="javascript:;">Help</a></li>
-            <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+            <li><a href="{{url('/logout')}}"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
           </ul>
         </li>
+        @endif
 
         <li role="presentation" class="dropdown">
           <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
