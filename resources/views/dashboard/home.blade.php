1875aa655ff29e8c53076bb41fc41c8d7f8d390f
faizalruzky <faizalruzky@gmail.com>
2016-09-22 19:51:58 +0700

    First Commit

---
+++ resources/views/dashboard/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.application')
+@section('content')
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="row">
+          <h3>Welcome Admin</h3>
+          </div>
+          <br/>
+          <img src="assets/images/quran_logo.png" class="img-responsive center-block" alt="Responsive image" width=50% height="auto">
+        </div>
+        <!-- /page content --> 
+@endsection
+
+
