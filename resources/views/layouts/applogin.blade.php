40ef2e5d216409548b56790091f7c5398f700cfa
faizalruzky <faizalruzky@gmail.com>
2016-09-23 22:35:25 +0700

    First Commit, add file app login for source login css

---
+++ resources/views/layouts/applogin.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Dashboard Admin</title>
+
+    <!-- fonts -->
+    <!-- awesome -->
+    <link href="/assets/fonts/font-awesome.min.css" rel="stylesheet">
+
+    <!-- materialize -->
+    <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+    <!-- end fonts -->
+
+    <!-- styles -->
+    <!-- bootstrap -->
+    <link href="/assets/library/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- materialize -->
+    <!-- <link rel="stylesheet" type="text/css" href="/assets/library/materialize/css/materialize.min.css">  -->   
+
+    <!-- end styles -->
+
+    <!-- nprogress -->
+    <link href="/assets/library/nprogress/nprogress.css" rel="stylesheet">
+    <!-- end nprogress -->
+
+    <!-- animate -->
+    <link href="/assets/library/animate/animate.min.css" rel="stylesheet">
+    <!-- end animate -->
+    
+    <!-- custom -->
+    <link href="/assets/css/custom.min.css" rel="stylesheet">
+
+</head>
+<body class="login">
+
+@yield('content')
+
+</body>
+
+</html>
