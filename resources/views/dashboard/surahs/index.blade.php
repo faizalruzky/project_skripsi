595d6626436fe7909996d1cbbf489fa17f0aa1c1
faizalruzky <faizalruzky@gmail.com>
2016-09-25 19:26:28 +0700

    edit coloum no surat

--- resources/views/dashboard/surahs/index.blade.php
+++ resources/views/dashboard/surahs/index.blade.php
@@ -5,7 +5,7 @@
   <div class="row">
     <ol class="breadcrumb">
       <li><a href="{{ url('/administrator') }}">Dashboard</a></li>
-      <li><a href="{{ url('administrator/surahs') }}">Surahs</a></li>
+      <li>Surahs</li>
     </ol>
     <div class="panel panel-info">
       <div class="panel-heading">
@@ -18,7 +18,7 @@
         <div class="table-responsive">
           <table class="table table-bordered table-striped table-hover">                
             <tr>
-              <th>No Ayat</th>
+              <th>No Surat</th>
               <th>Nama Surat</th>          
               <th>Option</th>
             </tr>
@@ -26,7 +26,7 @@
             @foreach ($surahs as $surah)
             {{-- */$x++;/* --}}
             <tr>
-              <td>{{ $x }}</td>
+              <td>{{ $surah->id }}</td>
               <td><a href="{{ url('administrator/surahs',$surah->id) }}">{{ $surah->nama_surat }} <h4>{{ $surah->arab_surat }}</h4> {{ $surah->arti_surat }}</a></td>
               <div>
                 <td>
