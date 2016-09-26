c540d43f9bdfa44f9b853e7ab9e341a6840aa40c
faizalruzky <faizalruzky@gmail.com>
2016-09-26 23:57:16 +0700

    add js library validator and script validator

--- resources/views/layouts/application.blade.php
+++ resources/views/layouts/application.blade.php
@@ -86,6 +86,10 @@
 
     <script src="/assets/library/datatables/js/jquery.dataTables.min.js"></script>
 
+    <!-- validator -->
+    <script src="/assets/library/validator/validator.js"></script>
+    <!-- end -->
+
     <!-- custom -->
     <script src="/assets/js/custom.min.js"></script>
     <script src="/assets/js/custom.js"></script>
@@ -138,6 +142,39 @@
       gauge.setTextField(document.getElementById("gauge-text"));
     </script>
     <!-- /gauge.js -->
+
+    <!-- validator -->
+    <script>
+      // initialize the validator function
+      validator.message.date = 'not a real date';
+
+      // validate a field on "blur" event, a 'select' on 'change' event & a '.reuired' classed multifield on 'keyup':
+      $('form')
+        .on('blur', 'input[required], input.optional, select.required', validator.checkField)
+        .on('change', 'select.required', validator.checkField)
+        .on('keypress', 'input[required][pattern]', validator.keypress);
+
+      $('.multi.required').on('keyup blur', 'input', function() {
+        validator.checkField.apply($(this).siblings().last()[0]);
+      });
+
+      $('form').submit(function(e) {
+        e.preventDefault();
+        var submit = true;
+
+        // evaluate the form using generic validaing
+        if (!validator.checkAll($(this))) {
+          submit = false;
+        }
+
+        if (submit)
+          this.submit();
+
+        return false;
+      });
+    </script>
+    <!-- /validator -->
+
 </body>
 <footer>
  @include('dashboard/shared.footer')
