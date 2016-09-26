c6686ba0d53702222244e5dfea4340396b708f6c
faizalruzky <faizalruzky@gmail.com>
2016-09-26 23:54:55 +0700

    return to old index

--- resources/views/dashboard/surahs/index.blade.php
+++ resources/views/dashboard/surahs/index.blade.php
@@ -3,6 +3,12 @@
 @section('content')
 <div class="right_col" role="main">
   <div class="row">
+    <div class="input-group">
+      <input type="text" class="form-control">
+      <span class="input-group-btn">
+        <button type="button" class="btn btn-primary">Go!</button>
+      </span>
+    </div>
     <ol class="breadcrumb">
       <li><a href="{{ url('/administrator') }}">Dashboard</a></li>
       <li>Surahs</li>
@@ -16,12 +22,38 @@
       </div>
       <div class="panel-body">
         <div id="surahs-list">
-          @include('dashboard/surahs.list')
+          <div class="table-responsive">
+          <table class="table table-bordered table-striped table-hover">                
+            <tr>
+              <th>No Surat</th>
+              <th>Nama Surat</th>          
+              <th>Option</th>
+            </tr>
+            {{-- */$x=0;/* --}}
+            @foreach ($surahs as $surah)
+            {{-- */$x++;/* --}}
+            <tr>
+              <td>{{ $surah->id }}</td>
+              <td><a href="{{ url('administrator/surahs',$surah->id) }}">{{ $surah->nama_surat }} <h4>{{ $surah->arab_surat }}</h4> {{ $surah->arti_surat }}</a></td>
+              <div>
+                <td>
+                  <a class="btn btn-info" href="{{ action('SurahsController@edit', $surah->id) }}"><i class="fa fa-pencil"></i> Edit</a>
+
+                  {!! Form::open(['action' => ['SurahsController@destroy', $surah->id], 'method' => 'DELETE']) !!}
+                  <button type="submit" class="btn btn-danger"><i class="fa fa-trash"></i> Delete</button>
+                  {!! Form::close() !!}
+                </td>
+              </div>
+            </tr>
+            @endforeach
+          </table>
+          {!! $surahs->render() !!}
+        </div>
         </div>
       </div>
     </div>
     <!-- ================================================== -->
   </div>
 </div>
-<script src="/assets/js/custom.js"></script>
+<script src="/assets/js/custom1.js"></script>
 @endsection
