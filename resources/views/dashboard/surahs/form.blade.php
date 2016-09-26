8852b941b8a16245961384a8795c0f8d130b6747
faizalruzky <faizalruzky@gmail.com>
2016-09-26 23:53:47 +0700

    now, this file not using

--- resources/views/dashboard/surahs/form.blade.php
+++ resources/views/dashboard/surahs/form.blade.php
@@ -1,4 +1,7 @@
-    	<div class="form-group">
+        <div class="x_panel">
+                <div class="x_content">
+                    <br />
+        <div class="form-group" >
             {!! Form::label("nama_surat", "Nama Surat:") !!}
             {!! Form::text("nama_surat", null, ["class" => "form-control"]) !!}
 
@@ -11,6 +14,10 @@
         </div>
             
 
-    	<div class="form-actions">
+        <div class="form-actions">
             <button type="submit" name="submit" class="btn btn-primary">{!! $submitText !!}</button>
-    	</div>
+        </div>
+        </div>
+        </div>
+
+
