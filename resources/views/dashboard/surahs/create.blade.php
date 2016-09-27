93748fa6731854d3d4f2abe72ed88645751a77b7
faizalruzky <faizalruzky@gmail.com>
2016-09-27 23:18:15 +0700

    edit view form create

--- resources/views/dashboard/surahs/create.blade.php
+++ resources/views/dashboard/surahs/create.blade.php
@@ -22,7 +22,7 @@
 	<hr>
 	<ol class="breadcrumb">
 		<li><a href="{{ url('/administrator') }}">Dashboard</a></li>
-		<li><a href="{{ url('administrator/surahs') }}">Surahs</a></li>
+		<li><a href="{{ url('administrator/surahs') }}">Surat</a></li>
 		<li>Create</li>
 	</ol>
 
@@ -42,39 +42,9 @@
 						<div class="clearfix"></div>
 					</div>
 					<div class="x_content">
-
-						<form class="form-horizontal form-label-left" novalidate role="form" method="POST" action="{{ url('administrator/surahs') }}">
-							{{ csrf_field() }}         
-							<div class="item form-group">
-								<label class="control-label col-md-3 col-sm-3 col-xs-12" for="nama_surat">Nama Surat <span class="required">*</span>
-								</label>
-								<div class="col-md-6 col-sm-6 col-xs-12">
-								<div class="form-group">
-									<input class="form-control col-md-7 col-xs-12" name="nama_surat" type="text" required="required">
-									</div>
-								</div>
-							</div>
-							<div class="item form-group">
-								<label class="control-label col-md-3 col-sm-3 col-xs-12" for="arab_surat">Arab Surat <span class="required">*</span>
-								</label>
-								<div class="col-md-6 col-sm-6 col-xs-12">
-									<input type="text" name="arab_surat" class="form-control col-md-7 col-xs-12" required="required">
-								</div>
-							</div>
-							<div class="item form-group">
-								<label class="control-label col-md-3 col-sm-3 col-xs-12" for="arti_surat">Arti Surat <span class="required">*</span>
-								</label>
-								<div class="col-md-6 col-sm-6 col-xs-12">
-									<input type="text" name="arti_surat" class="form-control col-md-7 col-xs-12" required="required">
-								</div>
-							</div>
-							<div class="ln_solid"></div>
-							<div class="form-group">
-								<div class="col-md-6 col-md-offset-3">
-									<button type="submit" class="btn btn-primary">Submit</button>
-								</div>
-							</div>
-						</form>
+						{!! Form::open(['url' => 'administrator/surahs', 'class' => 'form-horizontal form-label-left','role' => 'form', 'enctype'=>"multipart/form-data" ]) !!}         
+						@include('dashboard/surahs.form')
+						{!! Form::close() !!}
 					</div>
 				</div>
 			</div>
