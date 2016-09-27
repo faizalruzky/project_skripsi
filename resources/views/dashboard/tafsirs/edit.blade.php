674af23039f619475a53e88b762f4a0e47219797
faizalruzky <faizalruzky@gmail.com>
2016-09-27 23:23:05 +0700

    First Commit, add edit for edit tafsir

---
+++ resources/views/dashboard/tafsirs/edit.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.application')
+
+@section('content')
+<div class="right_col" role="main">
+  <div class="row">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Tafsir Quraish Syihab</h3>
+      </div>
+
+      <div class="title_right">
+        <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search for...">
+            <span class="input-group-btn">
+              <button class="btn btn-default" type="button">Go!</button>
+            </span>
+          </div>
+        </div>
+      </div>
+      </div>
+      <ol class="breadcrumb">
+        <li><a href="{{ url('/administrator') }}">Dashboard</a></li>
+        <li><a href="{{ url('/administrator/tafsirs') }}">Tafsirs</a></li>
+        <li>{!!$surah->nama_surat!!}</li>
+      </ol>
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          <h3 class="visible-lg-inline visible-md-inline">Tafsir Surat {!!$surah->arab_surat!!} ({!!$surah->arti_surat!!})</h3>
+          <a href="{{ url('administrator/tafsirs',$qurantafsir->ayat_id) }}" class="btn btn-primary pull-right">
+            <i class="fa fa-plus"></i> Add Tafsirs
+          </a>
+        </div>
+        <div class="panel-body">
+          <div class="table-responsive">
+            <table class="table table-bordered table-striped table-hover">                
+              <tr>
+                <th>No Ayat</th>
+                <th>Tafsir</th>          
+                <th>Option</th>
+              </tr>
+              @foreach ($tafsirs as $tafsir)
+              <tr>
+                <td>{{ $tafsir->ayat_id }}</td>
+                <td>
+                  {!! Form::model($tafsir, ['method' => 'PATCH', 'action' => ['TafsirsController@update', $tafsir->id]]) !!}
+                  {!! Form::textarea('tafsir',null,array('class'=>'span12 form-control','placeholder'=>'update tafsir disini','rows'=>'5','value'=>'{{ $tafsir->tafsir }}'))!!}
+                  
+                </td>
+                <div>
+                  <td>
+                    {!! Form::submit('Save', array('class' => 'btn btn-primary')) !!}
+                    {!! Form::close() !!}
+                    {!! Form::open(['action' => ['SurahsController@destroy', $tafsir->id], 'method' => 'DELETE']) !!}
+                    <button type="submit" class="btn btn-danger"><i class="fa fa-trash"></i> Delete</button>
+                    {!! Form::close() !!}
+                  </td>
+                </div>
+              </tr>
+              @endforeach
+            </table>
+            {!! $tafsirs->links() !!}
+          </div>
+        </div>
+      </div>
+      <!-- ================================================== -->
+    </div>
+  </div>
+</div>
+@endsection
