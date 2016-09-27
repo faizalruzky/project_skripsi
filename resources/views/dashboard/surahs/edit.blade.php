68981355fbd4e5c4abde87a75654e13a953b5578
faizalruzky <faizalruzky@gmail.com>
2016-09-27 23:18:44 +0700

    edit view form edit

--- resources/views/dashboard/surahs/edit.blade.php
+++ resources/views/dashboard/surahs/edit.blade.php
@@ -2,19 +2,53 @@
 
 @section('content')
 <div class="right_col" role="main">
-  <div class="row">
-    <h1>Edit Surah</h1>
-    <hr>
-    <ol class="breadcrumb">
-      <li><a href="{{ url('/administrator') }}">Dashboard</a></li>
-      <li><a href="{{ url('administrator/surahs') }}">Surahs</a></li>
-      <li>Edit</li>
-    </ol>
-    {!! Form::model($surah, ['method' => 'PATCH', 'action' => ['SurahsController@update', $surah->id]]) !!}
-    @include('dashboard/surahs.form', ['submitText' => '<i class="fa fa-check"></i> Update'])
-    {!! Form::close() !!}
+  <!-- {!! Form::open(['url' => 'administrator/surahs']) !!} -->
+  <div class="page-title">
+    <div class="title_left">
+      <h3>Create Surat</h3>
+    </div>
 
-    <!-- ================================================== -->
+    <div class="title_right">
+      <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Search for...">
+          <span class="input-group-btn">
+            <button class="btn btn-default" type="button">Go!</button>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr>
+  <ol class="breadcrumb">
+    <li><a href="{{ url('/administrator') }}">Dashboard</a></li>
+    <li><a href="{{ url('administrator/surahs') }}">Surat</a></li>
+    <li>Edit</li>
+  </ol>
+
+  <div class="">
+
+    <div class="clearfix"></div>
+
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2>Form</h2>
+            <ul class="nav navbar-right panel_toolbox">
+              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">  
+              {!! Form::model($surah, ['method' => 'PATCH','class' => 'form-horizontal form-label-left','role' => 'form', 'enctype'=>"multipart/form-data", 'action' => ['SurahsController@update', $surah->id]]) !!}    
+              @include('dashboard/surahs.form')
+              {!! Form::close() !!}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
-@stop
+@endsection
