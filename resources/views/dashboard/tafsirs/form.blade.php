5846eae670293603b5ca22d5b8673afc42e61c56
faizalruzky <faizalruzky@gmail.com>
2016-09-27 23:23:56 +0700

    form for create and edit, but not unuse yet

--- resources/views/dashboard/tafsirs/form.blade.php
+++ resources/views/dashboard/tafsirs/form.blade.php
@@ -1,16 +1,29 @@
-    	<div class="form-group" >
-            {!! Form::label("nama_surat", "Nama Surat:") !!}
-            {!! Form::text("nama_surat", null, ["class" => "form-control"]) !!}
-
-            {!! Form::label("arab_surat", "Arab Surat:") !!}
-            {!! Form::text("arab_surat", null, ["class" => "form-control"]) !!}
-
-            {!! Form::label("arti_surat", "Arti Surat:") !!}
-            {!! Form::text("arti_surat", null, ["class" => "form-control"]) !!}
-
+<div class="item form-group">
+    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="nama_surat">Nama Surat <span class="required">*</span>
+    </label>
+    <div class="col-md-6 col-sm-6 col-xs-12">
+        <div class="form-group">
+            <input class="form-control col-md-7 col-xs-12" name="nama_surat" type="text" required="required">
         </div>
-            
-
-    	<div class="form-actions">
-            <button type="submit" name="submit" class="btn btn-primary">{!! $submitText !!}</button>
-    	</div>
+    </div>
+</div>
+<div class="item form-group">
+    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="arab_surat">Arab Surat <span class="required">*</span>
+    </label>
+    <div class="col-md-6 col-sm-6 col-xs-12">
+        <input type="text" name="arab_surat" class="form-control col-md-7 col-xs-12" required="required">
+    </div>
+</div>
+<div class="item form-group">
+    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="arti_surat">Arti Surat <span class="required">*</span>
+    </label>
+    <div class="col-md-6 col-sm-6 col-xs-12">
+        <input type="text" name="arti_surat" class="form-control col-md-7 col-xs-12" required="required">
+    </div>
+</div>
+<div class="ln_solid"></div>
+<div class="form-group">
+    <div class="col-md-6 col-md-offset-3">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+</div>
