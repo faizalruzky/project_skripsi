6e4d0b74c8429ac736f4f3f54eb22949ff4cf370
faizalruzky <faizalruzky@gmail.com>
2016-10-06 19:23:51 +0700

    add stemming and search, status ok

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Pagination\Paginator;
 use App\Http\Requests;
 use App\Word;
+use App\Quran;
+use App\Surah;
+use DB;
 
 class SearchController extends Controller
 {
@@ -14,13 +18,37 @@ class SearchController extends Controller
     {
     	$stemmerFactory = new \Sastrawi\Stemmer\StemmerFactory();
 		$stemmer  = $stemmerFactory->createStemmer();
+		$dictionary = $stemmerFactory->createDefaultDictionary();
+		// $db = new Word();
+		// foreach ($db->query('SELECT katadasar FROM words') as $word) {
+		// 	$dictionary->add($word);
+		// }
+    
+    	$stemmer = new \Sastrawi\Stemmer\Stemmer($dictionary);
     	if($request->has('search')){
 
     		$output = $stemmer->stem($request->input('search'));
+            // $surah = $request->id;
+            // $surah = Surah::Where('id');      
+    		$qurans = Quran::search($output)->toArray();
 
     	}
-    	return view('public/search.index',compact('output'));
 
+    	return view('public/search.index',compact('qurans','output'));
+    	// return view('public/search.index')->with('qurans',$qurans);
+     //    ->with('surah',$surah);
+    }
+    public function create()
+    {
+    	
+    	return view('dashboard/words.create');
     }
     
+    public function store(Request $request)
+    {
+    	$db = new Word();
+    	$db->katadasar=$request->katadasar;
+    	$db->save();
+    	
+    }
 }
