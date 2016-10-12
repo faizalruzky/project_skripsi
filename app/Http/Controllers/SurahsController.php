b85f37a6af21c314078a3143c798ae3e6861045d
faizalruzky <faizalruzky@gmail.com>
2016-10-12 23:56:58 +0700

    OK

--- app/Http/Controllers/SurahsController.php
+++ app/Http/Controllers/SurahsController.php
@@ -13,6 +13,9 @@ use App\Quran;
 
 class SurahsController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth');
+    } 
     /**
      * Display a listing of the resource.
      *
