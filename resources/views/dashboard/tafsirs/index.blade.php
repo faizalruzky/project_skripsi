f85034657b6dd028dbf0bbfd5ee57732bf97a7d8
faizalruzky <faizalruzky@gmail.com>
2016-10-13 00:00:40 +0700

    add source jquery

--- resources/views/dashboard/tafsirs/index.blade.php
+++ resources/views/dashboard/tafsirs/index.blade.php
@@ -11,51 +11,45 @@
       <div class="title_right">
         <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
           <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search for...">
+            <input id="keywords" type="search" class="form-control" placeholder="Cari...">
             <span class="input-group-btn">
-              <button class="btn btn-default" type="button">Go!</button>
+              <button id="search" class="btn btn-default" type="button">Go!</button>
             </span>
           </div>
         </div>
       </div>
-      </div>
-      <ol class="breadcrumb">
-        <li><a href="{{ url('/administrator') }}">Dashboard</a></li>
-        <li>Tafsirs</li>
+    </div>
+    <ol class="breadcrumb">
+      <li><a href="{{ url('/administrator') }}"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a></li>
+      @if ( Request::input('q') )
+      {{-- */$query_params =  Request::input('q')/* --}}
+      <li class="active"><a href="{{url('administrator/surahs')}}">Surat</a></li>
+      <li class="active">condition(  
+
+        {{-- */ $loop = 0 /* --}}
+        @foreach( $query_params as $key => $value )
+        @if ( $loop != 0 ) / @endif {{ $key }} : {{ $value }}
+        {{-- */ $loop++ /* --}}
+        @endforeach
+        )</li>
+        @else
+        <li class="active">Tafsir</li>
+        @endif
       </ol>
       <div class="panel panel-info">
         <div class="panel-heading">
           <h3 class="visible-lg-inline visible-md-inline">Tafsir</h3>
         </div>
         <div class="panel-body">
-          <div class="table-responsive">
-            <table class="table table-bordered table-striped table-hover">                
-              <tr>
-                <th>No Surat</th>
-                <th>Nama Surat</th>          
-                <th>Option</th>
-              </tr>
-              @foreach ($surahs as $surah)
-              <tr>
-                <td>{{ $surah->id }}</td>
-                <td><a href="{{ url('administrator/tafsirs',$surah->id) }}">{{ $surah->nama_surat }} <h4>{{ $surah->arab_surat }}</h4> {{ $surah->arti_surat }}</a></td>
-                <div>
-                  <td>
-                  <a class="btn btn-info" href="{{ action('TafsirsController@edit', $surah->id) }}"><i class="fa fa-pencil"></i> Edit</a>
-
-                    {!! Form::open(['action' => ['SurahsController@destroy', $surah->id], 'method' => 'DELETE']) !!}
-                    <button type="submit" class="btn btn-danger"><i class="fa fa-trash"></i> Delete</button>
-                    {!! Form::close() !!}
-                  </td>
-                </div>
-              </tr>
-              @endforeach
-            </table>
-            {!! $surahs->links() !!}
+          <div id="surahs-list"> 
+            @include('dashboard/tafsirs.list')
           </div>
         </div>
       </div>
       <!-- ================================================== -->
     </div>
   </div>
-  @endsection
+</div>
+<script src="/assets/jquery/jquery.min.js"></script>
+<script src="/assets/js/ajax-custom.js"></script>
+@endsection
