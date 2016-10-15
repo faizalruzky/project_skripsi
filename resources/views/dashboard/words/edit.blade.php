92db8d0cc0b41fd19a709f26d25c06aeb8760f5d
faizalruzky <faizalruzky@gmail.com>
2016-10-15 20:22:17 +0700

    add edit for word

---
+++ resources/views/dashboard/words/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.application')
+
+@section('content')
+<div class="right_col" role="main">
+  <!-- {!! Form::open(['url' => 'administrator/surahs']) !!} -->
+  <div class="page-title">
+    <div class="title_left">
+      <h3>Edit Kata dasar</h3>
+    </div>
+  </div>
+  <hr>
+  <ol class="breadcrumb">
+    <li><a href="{{ url('/administrator') }}">Dashboard</a></li>
+    <li><a href="{{ url('administrator/words') }}">Kata dasar</a></li>
+    <li>Edit</li>
+  </ol>
+
+  <div class="">
+
+    <div class="clearfix"></div>
+
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2>Form</h2>
+            <ul class="nav navbar-right panel_toolbox">
+              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">  
+              {!! Form::model($word, ['method' => 'PATCH','class' => 'form-horizontal form-label-left','role' => 'form', 'enctype'=>"multipart/form-data", 'action' => ['WordsController@update', $word->id]]) !!}    
+              @include('dashboard/words.form')
+              {!! Form::close() !!}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
