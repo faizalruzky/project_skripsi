2a3e24bf75e317249a1d84c55c323a441a432bac
faizalruzky <faizalruzky@gmail.com>
2016-10-16 21:13:51 +0700

    add footer for public

---
+++ resources/views/public/shared/footer.blade.php
@@ -0,0 +1,8 @@
+<!-- footer content -->
+       
+          <div class="pull-right">
+            ©2016 - Faizal Ruzky
+          </div>
+          <div class="clearfix"></div>
+     
+        <!-- /footer content -->  
