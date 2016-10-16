9d855048ec62660b546bfb72ffb0206b2d738bd9
faizalruzky <faizalruzky@gmail.com>
2016-10-17 00:10:47 +0700

    view list surahs public, ok

--- resources/views/public/surahs/index.blade.php
+++ resources/views/public/surahs/index.blade.php
@@ -1,21 +1,71 @@
 @extends('layouts.apppublic')
 @section('content')
-<div class="container">
+<div class="container-fluid">
   <div class="row">
-    @foreach ($surahs as $surah)
+
     <div class="col-md-4 col-sm-4 col-xs-12">
-      <div class="list-group">
-      <table class="table table-hover">     
-        <a href="{{ url('administrator/surahs',$surah->id) }}" class="list-group">
-       <div class="clearfix visible-xs-block"></div>
-       <div class="col-md-1 col-sm-1 col-xs-1">{{ $surah->id }}</div>
-       <div class="col-md-8 col-sm-8 col-xs-8">{{ $surah->nama_surat }}<br>{{ $surah->arti_surat }}</div>
-       <h4>{{ $surah->arab_surat }}</h4>
-        </a>
-        </table> 
-      </div>
+      @foreach ($surahs as $surah)
+      @if ($surah->id==0)
+      @continue
+      @endif
+      <table class="table table-responsive table-hover"> 
+        <td>
+          <div class="list-group">
+            <a href="{{ url('administrator/surahs',$surah->id) }}">
+              <div class="col-md-1 col-sm-1 col-xs-1">{{ $surah->id }}</div>
+              <div class="col-md-8 col-sm-8 col-xs-8">{{ $surah->nama_surat }}<br>{{ $surah->arti_surat }}</div>
+              <h4 class="pull-right">{{ $surah->arab_surat }}</h4>
+            </a>
+          </div>
+        </td>
+      </table>
+      @if ($surah->id==38)
+      @break
+      @endif
+      @endforeach
     </div>
-    @endforeach
+
+    <div class="col-md-4 col-sm-4 col-xs-12">
+      @foreach ($surahs as $surah)
+      @if ($surah->id< 39)
+      @continue
+      @endif
+      <table class="table table-responsive table-hover"> 
+        <td>
+          <div class="list-group">
+            <a href="{{ url('administrator/surahs',$surah->id) }}">
+              <div class="col-md-1 col-sm-1 col-xs-1">{{ $surah->id }}</div>
+              <div class="col-md-8 col-sm-8 col-xs-8">{{ $surah->nama_surat }}<br>{{ $surah->arti_surat }}</div>
+              <h4 class="pull-right">{{ $surah->arab_surat }}</h4>
+            </a>
+          </div>
+        </td>
+      </table>
+      @if ($surah->id==76)
+      @break
+      @endif
+      @endforeach
+    </div>
+
+    <div class="col-md-4 col-sm-4 col-xs-12">
+      @foreach ($surahs as $surah)
+      @if ($surah->id< 77)
+      @continue
+      @endif
+      <table class="table table-responsive table-hover"> 
+        <td>
+          <div class="list-group">
+            <a href="{{ url('administrator/surahs',$surah->id) }}">
+              <div class="col-md-1 col-sm-1 col-xs-1">{{ $surah->id }}</div>
+              <div class="col-md-8 col-sm-8 col-xs-8">{{ $surah->nama_surat }}<br>{{ $surah->arti_surat }}</div>
+              <h4 class="pull-right">{{ $surah->arab_surat }}</h4>
+            </a>
+          </div>
+        </td>
+      </table>
+      @endforeach
+    </div>
+
   </div>
 </div>
 @endsection
