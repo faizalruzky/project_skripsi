b98a785aa9a09c8e0ba71b2d95c128746a2d8634
faizalruzky <faizalruzky@gmail.com>
2016-10-18 00:12:47 +0700

    add side menu

--- resources/views/public/surahs/index.blade.php
+++ resources/views/public/surahs/index.blade.php
@@ -1,6 +1,9 @@
-@extends('layouts.apppublic')
+@extends('layouts.app')
 @section('content')
-<div class="container-fluid">
+
+<div class="container">
+ <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+ <h2>Surat</h2>
   <div class="row">
 
     <div class="col-md-4 col-sm-4 col-xs-12">
@@ -8,7 +11,7 @@
       @if ($surah->id==0)
       @continue
       @endif
-      <table class="table table-responsive table-hover"> 
+      <table style="border-color:white" class="table table-responsive table-hover"> 
         <td>
           <div class="list-group">
             <a href="{{ url('administrator/surahs',$surah->id) }}">
@@ -30,7 +33,7 @@
       @if ($surah->id< 39)
       @continue
       @endif
-      <table class="table table-responsive table-hover"> 
+      <table style="border-color:white" class="table table-responsive table-hover"> 
         <td>
           <div class="list-group">
             <a href="{{ url('administrator/surahs',$surah->id) }}">
@@ -52,11 +55,11 @@
       @if ($surah->id< 77)
       @continue
       @endif
-      <table class="table table-responsive table-hover"> 
+      <table style="border-color:white" class="table table-responsive table-hover"> 
         <td>
           <div class="list-group">
             <a href="{{ url('administrator/surahs',$surah->id) }}">
-              <div class="col-md-1 col-sm-1 col-xs-1">{{ $surah->id }}</div>
+              <div  class="col-md-1 col-sm-1 col-xs-1">{{ $surah->id }}</h5></div>
               <div class="col-md-8 col-sm-8 col-xs-8">{{ $surah->nama_surat }}<br>{{ $surah->arti_surat }}</div>
               <h4 class="pull-right">{{ $surah->arab_surat }}</h4>
             </a>
@@ -68,4 +71,5 @@
 
   </div>
 </div>
+
 @endsection
