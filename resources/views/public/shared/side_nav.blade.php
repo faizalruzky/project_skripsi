2199ff94d4cf62d450b7b3d8e49e8e1917e3d54a
faizalruzky <faizalruzky@gmail.com>
2016-10-18 00:11:38 +0700

    add side nav

---
+++ resources/views/public/shared/side_nav.blade.php
@@ -0,0 +1,10 @@
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">Home</a>
+  <a href="#">Quran</a>
+  <a href="#">Tafsir</a>
+  <a href="#">About</a>
+</div>
+<div id="main">
+@yield('content')
+</div>
