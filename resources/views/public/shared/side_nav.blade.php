4011ce46c4b1bc419785884b274a816d04d0adfc
faizalruzky <faizalruzky@gmail.com>
2016-10-18 20:35:10 +0700

    add side nav

--- resources/views/public/shared/side_nav.blade.php
+++ resources/views/public/shared/side_nav.blade.php
@@ -1,10 +1,15 @@
 <div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="#">Home</a>
-  <a href="#">Quran</a>
-  <a href="#">Tafsir</a>
-  <a href="#">About</a>
+	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+	<li><a href="{{url('/')}}"><i class="fa fa-home"></i> Home </a></li>
+	<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-book"></i> Quran <span class="fa fa-chevron-down"></span></a>
+		<ul class="dropdown-menu nav child_menu" role="menu">
+			<li><a href="{{url('/surahs')}}"><i class="glyphicon glyphicon-list-alt"></i> Detail Surat </a></li>
+			<li><a href="{{url('/tafsirs')}}"><i class="glyphicon glyphicon-book"></i> Tafsir </a></li>
+		</ul>
+		
+	</li>
+	<li><a href="#">About</a></li>
 </div>
 <div id="main">
-@yield('content')
+	@yield('content')
 </div>
