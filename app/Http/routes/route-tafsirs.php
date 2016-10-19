954929f958120df15262d7b7e143152d73cfd3ca
faizalruzky <faizalruzky@gmail.com>
2016-10-19 23:50:55 +0700

    add listtafsirs

--- app/Http/routes/route-tafsirs.php
+++ app/Http/routes/route-tafsirs.php
@@ -1,2 +1,3 @@
 <?php 
-Route::get('/tafsirs','TafsirsPublicController@index');
+Route::resource('/tafsirs','TafsirsPublicController');
+Route::get('/listtafsirs','TafsirsPublicController@listtafsirs');
