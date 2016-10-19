4ad6774560d34001ba90f56f28067d68350c85c8
faizalruzky <faizalruzky@gmail.com>
2016-10-19 23:57:06 +0700

    add list_surahs

---
+++ resources/views/public/surahs/list_surahs.blade.php
@@ -0,0 +1,22 @@
+@if (count($surahs))
+<div class="table-responsive">
+              <table class="table table-hover">                
+                <tr>
+                  <th class="text-center"><a href="" id="id">No Surat<i id="ic-direction"></i></a></th>
+                  <th class="text-center">Nama Surat</th>          
+                </tr>
+                {{-- */$x=0;/* --}}
+                @foreach ($surahs as $surah)
+                {{-- */$x++;/* --}}
+                <tr>
+                  <td class="text-center">{{ $surah->id }}</td>
+                  <td><a href="{{ url('/surahs',$surah->id) }}">{{ $surah->nama_surat }} <h4>{{ $surah->arab_surat }}</h4> {{ $surah->arti_surat }}</a></td>
+                </tr>
+                @endforeach
+              </table>
+            <input id="direction" type="hidden" value="asc" />
+
+            </div>
+            @else
+   <div class="card-panel red darken-3 white-text">Oops.. Data</b> Tidak Ditemukan</div>
+@endif
