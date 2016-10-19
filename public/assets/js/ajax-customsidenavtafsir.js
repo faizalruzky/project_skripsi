54797832936652115ea27b8ba3ae091612666e37
faizalruzky <faizalruzky@gmail.com>
2016-10-19 23:51:58 +0700

    add ajax nav tafsir

---
+++ public/assets/js/ajax-customsidenavtafsir.js
@@ -0,0 +1,155 @@
+$(function() {
+ $.ajaxSetup({
+                headers: {
+                    'X-XSRF-Token': $('meta[name="_token"]').attr('content')
+                }
+            });
+});
+
+$(document).ready(function() {
+
+  $(document).on('click', '.pagination a', function(e) {
+
+    get_pagesidetafsir($(this).attr('href').split('page=')[1]);
+
+    e.preventDefault();
+
+  });
+ }); 
+
+function get_pagesidetafsir(page) {
+
+  $.ajax({
+
+    url : '/listtafsirs?page=' + page,
+
+    type : 'GET',
+
+    dataType : 'json',
+
+    success : function(data) {
+
+      $('#sidetafsirs-list').html(data['view']);
+
+    },
+
+    error : function(xhr, status, error) {
+
+      console.log(xhr.error + "\n ERROR STATUS : " + status + "\n" + error);
+
+    },
+
+    complete : function() {
+
+      alreadyloading = false;
+
+    }
+
+  });
+
+}
+$('#searchtafsirside').on('click', function(){
+
+  $.ajax({
+
+    url : '/listtafsirs',
+
+    type : 'GET',
+
+    dataType : 'json',
+
+    data : {
+
+      'keywords' : $('#keywords').val()
+
+    },
+
+    success : function(data) {
+
+      $('#sidetafsirs-list').html(data['view']);
+
+    },
+
+    error : function(xhr, status) {
+
+      console.log(xhr.error + " ERROR STATUS : " + status);
+
+    },
+
+    complete : function() {
+
+      alreadyloading = false;
+
+    }
+
+  });
+
+});
+
+$(document).ready(function() {
+
+  $(document).on('click', '#id', function(e) {
+
+    sort_sidetafsir();
+
+    e.preventDefault();
+
+  });
+
+});
+
+function sort_sidetafsir() {
+
+  $('#id').on('click', function() {
+    var url = 
+
+    $.ajax({
+
+      url : '/listtafsirs',
+
+      typs : 'GET',
+
+      dataType : 'json',
+
+      data : {
+
+        "direction" : $('#direction').val()
+
+      },
+
+      success : function(data) {
+
+        $('#sidetafsirs-list').html(data['view']);
+
+        $('#direction').val(data['direction']);
+
+        if(data['direction'] == 'asc') {
+
+          $('i#ic-direction').attr({class: "fa fa-arrow-up"});
+
+        } else {
+
+          $('i#ic-direction').attr({class: "fa fa-arrow-down"});
+
+        }
+
+      },
+
+      error : function(xhr, status, error) {
+
+        console.log(xhr.error + "\n ERROR STATUS : " + status + "\n" + error);
+
+      },
+
+      complete : function() {
+
+        alreadyloading = false;
+
+      }
+
+    });
+
+  });
+
+}
+
