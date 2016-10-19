e48a1aa7f878fddb6a1be537facc0c39d475b944
faizalruzky <faizalruzky@gmail.com>
2016-10-19 23:59:24 +0700

    edit view index, ok

--- resources/views/public/tafsirs/index.blade.php
+++ resources/views/public/tafsirs/index.blade.php
@@ -7,61 +7,61 @@
   <div class="row">
 
     <div class="col-md-4 col-sm-4 col-xs-12">
-      @foreach ($surahs as $surah)
-      @if ($surah->id==0)
+      @foreach ($tafsirs as $tafsir)
+      @if ($tafsir->id==0)
       @continue
       @endif
       <table style="border: 0px solid transparent" class="table table-responsive table-hover"> 
         <td>
           <div class="list-group">
-            <a href="{{ url('administrator/surahs',$surah->id) }}">
-              <div class="col-md-1 col-sm-1 col-xs-1">{{ $surah->id }}</div>
-              <div class="col-md-8 col-sm-8 col-xs-8">{{ $surah->nama_surat }}<br>{{ $surah->arti_surat }}</div>
-              <h4 class="pull-right">{{ $surah->arab_surat }}</h4>
+            <a href="{{ url('/tafsirs',$tafsir->id) }}">
+              <div class="col-md-1 col-sm-1 col-xs-1">{{ $tafsir->id }}</div>
+              <div class="col-md-8 col-sm-8 col-xs-8">{{ $tafsir->nama_surat }}<br>{{ $tafsir->arti_surat }}</div>
+              <h4 class="pull-right">{{ $tafsir->arab_surat }}</h4>
             </a>
           </div>
         </td>
       </table>
-      @if ($surah->id==38)
+      @if ($tafsir->id==38)
       @break
       @endif
       @endforeach
     </div>
 
     <div class="col-md-4 col-sm-4 col-xs-12">
-      @foreach ($surahs as $surah)
-      @if ($surah->id< 39)
+      @foreach ($tafsirs as $tafsir)
+      @if ($tafsir->id< 39)
       @continue
       @endif
       <table style="border-color:white" class="table table-responsive table-hover"> 
         <td>
           <div class="list-group">
-            <a href="{{ url('administrator/surahs',$surah->id) }}">
-              <div class="col-md-1 col-sm-1 col-xs-1">{{ $surah->id }}</div>
-              <div class="col-md-8 col-sm-8 col-xs-8">{{ $surah->nama_surat }}<br>{{ $surah->arti_surat }}</div>
-              <h4 class="pull-right">{{ $surah->arab_surat }}</h4>
+            <a href="{{ url('/tafsirs',$tafsir->id) }}">
+              <div class="col-md-1 col-sm-1 col-xs-1">{{ $tafsir->id }}</div>
+              <div class="col-md-8 col-sm-8 col-xs-8">{{ $tafsir->nama_surat }}<br>{{ $tafsir->arti_surat }}</div>
+              <h4 class="pull-right">{{ $tafsir->arab_surat }}</h4>
             </a>
           </div>
         </td>
       </table>
-      @if ($surah->id==76)
+      @if ($tafsir->id==76)
       @break
       @endif
       @endforeach
     </div>
 
     <div class="col-md-4 col-sm-4 col-xs-12">
-      @foreach ($surahs as $surah)
-      @if ($surah->id< 77)
+      @foreach ($tafsirs as $tafsir)
+      @if ($tafsir->id< 77)
       @continue
       @endif
       <table style="border-color:white" class="table table-responsive table-hover"> 
         <td>
           <div class="list-group">
-            <a href="{{ url('administrator/surahs',$surah->id) }}">
-              <div  class="col-md-1 col-sm-1 col-xs-1">{{ $surah->id }}</h5></div>
-              <div class="col-md-8 col-sm-8 col-xs-8">{{ $surah->nama_surat }}<br>{{ $surah->arti_surat }}</div>
-              <h4 class="pull-right">{{ $surah->arab_surat }}</h4>
+            <a href="{{ url('/tafsirs',$tafsir->id) }}">
+              <div  class="col-md-1 col-sm-1 col-xs-1">{{ $tafsir->id }}</h5></div>
+              <div class="col-md-8 col-sm-8 col-xs-8">{{ $tafsir->nama_surat }}<br>{{ $tafsir->arti_surat }}</div>
+              <h4 class="pull-right">{{ $tafsir->arab_surat }}</h4>
             </a>
           </div>
         </td>
