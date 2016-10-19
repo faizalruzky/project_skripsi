8456d76d96ace60d9042df16081954901ff3c1bb
faizalruzky <faizalruzky@gmail.com>
2016-10-20 00:01:47 +0700

    add sidesurahs for tafsirs

---
+++ resources/views/public/tafsirs/sidesurahs.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.apppublictafsir')
+
+@section('content')
+<div class="right_col" role="main">
+  <div class="row">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Surat</h3>
+      </div>
+    </div>
+   
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          <h3 class="visible-lg-inline visible-md-inline">Surat</h3>
+          
+        </div>
+        <div class="panel-body">
+          <div id="sidetafsirs-list"> 
+            @include('public/tafsirs.list')
+          </div>
+        </div>
+      </div>
+
+      <!-- ================================================== -->
+    </div>
+  </div>
+</div>
+<script src="/assets/jquery/jquery.min.js"></script>
+<script src="/assets/js/ajax-customsidenavtafsir.js"></script>
+@endsection
