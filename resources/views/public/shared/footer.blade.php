6c13a85e83184b9e24f5de2f41d4a97987b6c33a
faizalruzky <faizalruzky@gmail.com>
2016-10-20 15:49:45 +0700

    footer, ok

--- resources/views/public/shared/footer.blade.php
+++ resources/views/public/shared/footer.blade.php
@@ -1,8 +1,37 @@
 <!-- footer content -->
-       
-          <div class="pull-right">
-            ©2016 - Faizal Ruzky
-          </div>
-          <div class="clearfix"></div>
+          <div id="footerOuterSeparator"></div>
+
+<div id="divFooter" class="footerArea">
+
+    <div class="container">
+
+        <div class="divPanel">
+
+            <div class="row-fluid">
+                
+                <div class="col-md-4 col-md-offset-4">
+                    <a href="mailto:faizal.ruzky@student.uinsgd.ac.id" title="Email"><p class="text-center">faizal.ruzky@student.uinsgd.ac.id</p></a>
+                    
+                </div>
+            </div>
+
+            <br />
+            <div class="col-md-4 col-md-offset-4">
+            <div class="row-fluid">
+                <div class="span12">
+                    <p class="copyright text-center">
+                    <small> Teknik Informatika - UIN Sunan Gunung Djati Bandung</small><br>
+                        Copyright © 2016 Faizal Ruzky. All Rights Reserved.
+                    </p>
+                </div>
+            </div>
+            </div>
+            <br />
+
+        </div>
+
+    </div>
+    
+</div>
      
         <!-- /footer content -->  
