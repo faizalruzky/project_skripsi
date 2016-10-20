3f3936e6d12dc31bbf7d165123b73dfc47c0d082
faizalruzky <faizalruzky@gmail.com>
2016-10-20 15:48:56 +0700

    add view about

---
+++ resources/views/public/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container">
+ <span class="pull-right" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+ <h2>About</h2>
+  <div class="row">
+  <p class="text-justify">Ini merupakan website hasil penelitian tugas akhir (skripsi), dengan tema "search engine ayat al-Qur'an" yang dibangun dengan Framework Laravel. Mudah-mudahan website ini bermanfaat bagi umat muslim, khususnya di Indonesia</p>
+  </div>
+</div>
+
+@endsection
