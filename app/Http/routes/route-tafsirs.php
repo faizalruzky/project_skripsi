d09730250677f824ba5832024eaa299998faa21e
faizalruzky <faizalruzky@gmail.com>
2016-11-07 23:35:32 +0700

    add listjalalayn, jalalayn, jalalayn{id}

--- app/Http/routes/route-tafsirs.php
+++ app/Http/routes/route-tafsirs.php
@@ -1,3 +1,6 @@
 <?php 
 Route::resource('/tafsirs','TafsirsPublicController');
 Route::get('/listtafsirs','TafsirsPublicController@listtafsirs');
+Route::get('/listjalalayn','TafsirsPublicController@listjalalayn');
+Route::get('/jalalayn','TafsirsPublicController@jalalayn');
+Route::get('/jalalayn/{id}','TafsirsPublicController@showjalalayn');
