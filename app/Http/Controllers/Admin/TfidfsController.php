334dc94da1a5698fb026b96f911cd9da301588be
faizalruzky <faizalruzky@gmail.com>
2016-11-07 23:34:27 +0700

    ok

--- app/Http/Controllers/Admin/TfidfsController.php
+++ app/Http/Controllers/Admin/TfidfsController.php
@@ -40,7 +40,7 @@ class TfidfsController extends Controller
         
         return view('dashboard/tfidfs.index',compact('freq','n','resBobot','num_rows','w','tfidf'));
     }
-    public function similarity (){
+    public function weight (){
         while($resBobot) {
             //$w = tf * log (n/N)
                 $term = $freq;
@@ -56,5 +56,48 @@ class TfidfsController extends Controller
                 $resUpdateBobot->update();  
             }
             $resBobot->toArray();
+    }
+      public function weightedtree($tree_terjemah, $tree_tafsir) {
+        $alldoc = $this->quran_model->view_all_doc();
+        $arraycos = array();
+        // $arraytanim = array();
+        $arraybtwc = array();
+        $i = 0;
+        $j = 0;
+        $k = 0;
+        foreach ($alldoc->result() as $val):
+
+            // $tree_surat = unserialize($val->tree_surat);
+            $tree_terjemah = unserialize($val->tree_terjemah);
+            $tree_tafsir = unserialize($val->tree_tafsir);
+           
+            $bterjemah = $this->cosine($tree_terjemah, $tree_keyterjemah);
+            $bkey = $this->cosine($tree_keyword, $tree_keyterjemah);
+            $btaf = $this->cosine($tree_tafsir, $tree_keytaf);
+
+         
+           (real) $sim = $bterjemah[0] * 0.5 + $bkey[0] * 0.5 + $btaf[0] * 0.5;
+           
+           (real) $simbtwc = $bterjemah[1] * 0.5 + $bkey[1] * 0.5 + $btaf[1] * 0.5;
+            
+      
+            
+            if ($simcos != 0 && $simcos != ($simcos>0.25)) {
+                $arraycos[$i]['sim'] = $simcos;
+                $arraycos[$i]['id'] = $val->id_quran;
+                $i++;
+            }
+            
+            if ($simbtwc != 0 && $simbtwc != (sqrt($simbtwc*$simbtwc)>0.01)) {
+                $arraybtwc[$k]['sim'] = sqrt($simbtwc*$simbtwc);
+                $arraybtwc[$k]['id'] = $val->id_quran;
+                $k++;
+            }
+            
+        endforeach;
+        
+        $arrayhasil = array($arraycos,$arraybtwc);
+        
+        return $arrayhasil;
     }
 }
