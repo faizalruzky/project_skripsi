7e878f593aaba60c890bf1b091eca0558b68a914
faizalruzky <faizalruzky@gmail.com>
2016-12-08 21:14:10 +0700

    disable option

--- resources/views/dashboard/tafsirs/list.blade.php
+++ resources/views/dashboard/tafsirs/list.blade.php
@@ -4,7 +4,7 @@
                 <tr>
                   <th class="text-center"><a href="" id="id">No Surat<i id="ic-direction"></i></a></th>
                   <th class="text-center">Nama Surat</th>          
-                  <th class="text-center">Option</th>
+                 <!--  <th class="text-center">Option</th> -->
                 </tr>
                 {{-- */$x=0;/* --}}
                 @foreach ($tafsirs as $tafsir)
@@ -12,14 +12,14 @@
                 <tr>
                   <td class="text-center">{{ $tafsir->id }}</td>
                   <td><a href="{{ url('administrator/tafsirs',$tafsir->id) }}">{{ $tafsir->nama_surat }} <h4>{{ $tafsir->arab_surat }}</h4> {{ $tafsir->arti_surat }}</a></td>
-                  <div>
+                 <!--  <div>
                     <td class="text-center">
                       <a class="btn btn-info" href="{{ action('Admin\TafsirsController@edit', $tafsir->id) }}"><i class="fa fa-pencil"></i> Edit</a>
 
                       {!! Form::open(['action' => ['Admin\TafsirsController@destroy', $tafsir->id], 'method' => 'DELETE','style' => 'display:inline']) !!}
                       <button type="submit" class="btn btn-danger"><i class="fa fa-trash"></i> Delete</button>
                       {!! Form::close() !!}
-                    </td>
+                    </td> -->
                   </div>
                 </tr>
                 @endforeach
